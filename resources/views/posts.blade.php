3bf4055b8cc685704bcbb8ff9cb05332ba8f8264
omari <omari.jalagania@gmail.com>
2022-10-05 21:01:55 +0400

    feat: add layouts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Blog</title>
-
-</head>
-
-<body>
+<x-layout>
     <article class="max-w-3xl mx-auto p-10">
         @foreach ($posts as $post)
-            <?= $post ?>
+            {!! $post !!}
         @endforeach
     </article>
-
-</body>
-
-</html>
+</x-layout>
